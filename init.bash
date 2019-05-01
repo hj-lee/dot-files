87b7aa85822f9c90595922ad47ddf4f70b2804b2
Hwijae Lee <hj.d.lee@gmail.com>
2019-04-30 20:43:49 -0400

    Move google-clould-sdk init to specific shells

---
+++ init.bash
@@ -0,0 +1,6 @@
+# The next line updates PATH for the Google Cloud SDK.
+source '/home/hjlee/usr/google-cloud-sdk/path.bash.inc'
+
+# The next line enables shell command completion for gcloud.
+source '/home/hjlee/usr/google-cloud-sdk/completion.bash.inc'
+
---
+++ init.zsh
@@ -0,0 +1,6 @@
+# The next line updates PATH for the Google Cloud SDK.
+source '/home/hjlee/usr/google-cloud-sdk/path.zsh.inc'
+
+# The next line enables shell command completion for gcloud.
+source '/home/hjlee/usr/google-cloud-sdk/completion.zsh.inc'
+
