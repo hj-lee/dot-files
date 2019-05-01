ac525fd69536821ce1d0c953a9534ac6e2d37726
Hwijae Lee <hj.d.lee@gmail.com>
2019-04-30 20:48:14 -0400

    Add zsh_command_not_found to init.zsh

--- init.zsh
+++ init.zsh
@@ -4,3 +4,7 @@ source '/home/hjlee/usr/google-cloud-sdk/path.zsh.inc'
 # The next line enables shell command completion for gcloud.
 source '/home/hjlee/usr/google-cloud-sdk/completion.zsh.inc'
 
+
+. /etc/zsh_command_not_found
+
+
