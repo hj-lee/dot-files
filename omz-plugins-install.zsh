0b1f639905a1726f78ee1eeacfe2b50fdb601294
Hwijae Lee <hj.d.lee@gmail.com>
2019-05-01 13:33:06 -0400

    Add omz-plugins-install.zsh

---
+++ omz-plugins-install.zsh
@@ -0,0 +1,5 @@
+
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
