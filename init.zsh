7ad74e69f8b4c0e574237dc06e95bd007b69850c
Hwijae Lee <hj.d.lee@gmail.com>
2019-11-17 08:21:45 -0500

    Set DEFAULT_USER

--- init.zsh
+++ init.zsh
@@ -4,6 +4,8 @@ source '/home/hjlee/usr/google-cloud-sdk/path.zsh.inc'
 # The next line enables shell command completion for gcloud.
 source '/home/hjlee/usr/google-cloud-sdk/completion.zsh.inc'
 
+DEFAULT_USER=hjlee
+
 unsetopt AUTO_CD
 
 # . /etc/zsh_command_not_found
