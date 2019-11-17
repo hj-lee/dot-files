b731357c643a3eb061cd09fed0ba57b6da371cad
Hwijae Lee <hj.d.lee@gmail.com>
2019-11-17 08:21:34 -0500

    Add z plugin

--- dot-zshrc.zsh
+++ dot-zshrc.zsh
@@ -2,7 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH="/home/hjlee/.oh-my-zsh"
+export ZSH="/home/hjlee/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
@@ -72,6 +72,7 @@ plugins=(
     web-search
     zsh-syntax-highlighting
     zsh-autosuggestions
+    z
 )
 
 source $ZSH/oh-my-zsh.sh
