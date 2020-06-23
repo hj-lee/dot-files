f36931946ca27fc2f7e36cf3f5a1aa567cbcd7b9
Hwijae Lee <hj.d.lee@gmail.com>
2020-06-23 18:11:35 -0400

    Remove system specific codes

--- init.bash
+++ init.bash
@@ -2,11 +2,3 @@
 function lpath() {
     echo "${PATH//:/$'\n'}"
 }
-
-
-# The next line updates PATH for the Google Cloud SDK.
-source '/home/hjlee/usr/google-cloud-sdk/path.bash.inc'
-
-# The next line enables shell command completion for gcloud.
-source '/home/hjlee/usr/google-cloud-sdk/completion.bash.inc'
-
--- init.zsh
+++ init.zsh
@@ -1,15 +1,6 @@
 
 alias lpath='echo "${PATH//:/\n}"'
 
-
-# The next line updates PATH for the Google Cloud SDK.
-source '/home/hjlee/usr/google-cloud-sdk/path.zsh.inc'
-
-# The next line enables shell command completion for gcloud.
-source '/home/hjlee/usr/google-cloud-sdk/completion.zsh.inc'
-
-DEFAULT_USER=hjlee
-
 unsetopt AUTO_CD
 
 # . /etc/zsh_command_not_found
