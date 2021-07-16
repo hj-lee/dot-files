0558a519b87c81e51e3960bc1bd19d45c3f6b477
Hwijae Lee <hj.d.lee@gmail.com>
2021-07-16 12:05:31 -0400

    Include date and time in prompt

--- init.zsh
+++ init.zsh
@@ -11,3 +11,7 @@ alias -g G='| grep'
 alias -g Gi='| grep -i'
 
 alias -g L='| less'
+
+# PROMPT="%{%f%b%k%}$(build_prompt) " 
+# PROMPT=$'%D %*\n%{%f%b%k%}$(build_prompt) '
+PROMPT=$'%F{cyan}%D %*\n%{%f%b%k%}$(build_prompt) '
