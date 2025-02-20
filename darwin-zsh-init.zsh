f9defe19a409424d0c4f0ab28488075019e2836a
Hwijae Lee <hj.d.lee@gmail.com>
2025-02-19 20:42:03 -0500

    Add commented out homebrew ruby env exports

--- darwin-zsh-init.zsh
+++ darwin-zsh-init.zsh
@@ -2,7 +2,6 @@
 alias emacs=/Applications/Emacs.app/Contents/MacOS/Emacs
 add-to-path-end /Applications/Emacs.app//Contents/MacOS/bin
 
-add-to-path /opt/homebrew/opt/ruby/bin
 add-to-path /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/
 add-to-path /Applications/Xcode.app/Contents/Developer/usr/bin
 
@@ -14,3 +13,8 @@ add-to-path-end ${ANDROID_SDK_ROOT}/tools
 add-to-path-end ${ANDROID_SDK_ROOT}/platform-tools
 
 add-to-path /Applications/Android\ Studio.app/Contents/MacOS/
+
+# homebrew ruby 
+add-to-path /opt/homebrew/opt/ruby/bin
+# export LDFLAGS="-L/opt/homebrew/opt/ruby/lib"
+# export CPPFLAGS="-I/opt/homebrew/opt/ruby/include"
